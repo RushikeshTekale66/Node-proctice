cbfc618e38c7069e2786e3ccd0cb469cbac9a651
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-02-15 20:29:28 +0530

    filter() example in Node

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const arr = [1,2,3,4,5,6,7,7,8,9];
 
 //filter is used to traverse all element of an array
-arr.filter((item)=>{
-    console.log(item);
+const result = arr.filter((item)=>{
+    return item>5;
 })
+
+console.log(result);
