dc91dfcc1337798185c7e8469bed23eb7a54aaf1
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-02-22 19:34:32 +0530

    CRUD FILE Update data in file

--- index.js
+++ index.js
@@ -6,4 +6,6 @@ const filePath = `${dirPath}/text.txt`;
 
 // fs.writeFileSync(filePath, "This is simple text file");
 
-fs.readFile(filePath, 'utf-8', (err, item) => { console.log(item); })
+// fs.readFile(filePath, 'utf-8', (err, item) => { console.log(item); })
+
+fs.appendFile(filePath, "And writer is rushikesh", (err) => { if (!err) { console.log("File is updated") } })
