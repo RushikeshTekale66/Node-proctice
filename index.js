2994e69918e38685135c1e8c0ec42f07d54fabb4
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-02-22 19:29:44 +0530

    CRUD FILE Read file

--- index.js
+++ index.js
@@ -4,4 +4,6 @@ const path = require('path');
 const dirPath = path.join(__dirname, 'Path');
 const filePath = `${dirPath}/text.txt`;
 
-fs.writeFileSync(filePath, "This is simple text file");
+// fs.writeFileSync(filePath, "This is simple text file");
+
+fs.readFile(filePath, 'utf-8', (err, item) => { console.log(item); })
