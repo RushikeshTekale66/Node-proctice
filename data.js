ff397239246ed1d661c2130554ddfe723ab2bd07
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-02-22 18:23:14 +0530

    Create file using cmd & add data init

--- data.js
+++
@@ -1,6 +0,0 @@
-const data = [
-    {name:"Rushikesh Tekale", moblile:7887388366},
-    {name:"Rohan Tekale", mobile:8263859585}
-]
-
-module.exports = data;
--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-const http = require('http');
-const data = require('./data')
+const fs = require('fs');
 
-http.createServer((req, res)=>{
-    res.writeHead(200, {'Content-Type':'application/json'});
-    res.write(JSON.stringify(data));
-    res.end;
-}).listen(5000);
+const input = process.argv;
+
+// PS C:\Users\Rushikesh Tekale\Web devlopment VS\Practice\Node> node index.js add data.txt "Rushikesh"
+// If it contain add in input the it will be create file name of data.txt & add data in it Rushikesh
+// in node process.argv already contain two file
+
+if(input[2]=='add'){
+    fs.writeFileSync(input[3], input[4]);
+}
+else{
+    console.log("Invalid input");
+}
