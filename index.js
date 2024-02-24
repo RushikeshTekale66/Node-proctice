aafea3362680529f545e69260c90240cbee1cbbf
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-02-24 20:25:27 +0530

    Sending file using routing & Adding default page to handle page not found error

--- index.js
+++ index.js
@@ -7,6 +7,21 @@ const app = express();
 const publicPath = path.join(__dirname, "public");
 
 // express.static is used to run the static file like html
-app.use(express.static(publicPath));
+// This process of accessing page cause an small error that it gives the extention name in query parameter to solve this we use express routing
+// app.use(express.static(publicPath));
+
+app.get((""), (req, res)=>{
+    res.sendFile(`${publicPath}/index.html`);
+})
+
+app.get(("/about"), (req, res)=>{
+    res.sendFile(`${publicPath}/about.html`)
+})
+
+
+// We can solve the page not found error by using this routing 
+app.get(("*"), (req, res)=>{
+    res.sendFile(`${publicPath}/nopage.html`);
+})
 
 app.listen(5000);
