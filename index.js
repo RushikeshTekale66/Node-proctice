347c165aa9b233218f2cfadb94277ead85e02808
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-02-26 19:31:28 +0530

    ejs in node.js to make node application dynamic

--- index.js
+++ index.js
@@ -3,12 +3,10 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// __dirname = Path of our current directory ex =  C:\Users\Rushikesh Tekale\Web devlopment VS\Practice\Node
 const publicPath = path.join(__dirname, "public");
 
-// express.static is used to run the static file like html
-// This process of accessing page cause an small error that it gives the extention name in query parameter to solve this we use express routing
-// app.use(express.static(publicPath));
+// Install npm i ejs 
+app.set('view engine', 'ejs');
 
 app.get((""), (req, res)=>{
     res.sendFile(`${publicPath}/index.html`);
@@ -16,6 +14,16 @@ app.get((""), (req, res)=>{
 
 app.get(("/about"), (req, res)=>{
     res.sendFile(`${publicPath}/about.html`)
+})
+
+// Making dynamic website using EJS
+app.get(("/profile"), (req, res)=>{
+    const data = {
+        name:"Rushikesh",
+        mobile:7887388366,
+        address:"Pune"
+    }
+   res.render('profile', {data});
 })
 
 
