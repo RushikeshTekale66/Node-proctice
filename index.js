43acdfc6e316ca22d1a2ba2f0db097b35239f8a6
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-02-27 19:20:50 +0530

    Middle ware : Route level middleware

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const app = express();
 
-// Application level middlewhere
-// It is applied for whole application
+// Router level middleware
+// It is applicable for specific route or how much route we define 
 
 const middleware = ((req, res, next)=>{
-    if(req.query.age>18){
+    if (!req.query.age){
+        res.send("<h1>Please provide your age! </h1>")
+    }
+    else if(req.query.age>18){
         res.send("<h1>Success</h1>");
     }
     else if(req.query.age<18){
@@ -16,11 +19,21 @@ const middleware = ((req, res, next)=>{
     }
     
 })
-
-app.use(middleware)
+// Application level middle where
+// app.use(middleware)
 
 app.get("/", (req, res)=>{
-    res.send("<h1>Hi I am Rushikesh</h1>")
+    res.send("<h1>Hi I am Rushikesh Home</h1>")
+})
+
+// Router level middleware , We can apply it to all route
+app.get("/profile", middleware , (req, res)=>{
+    res.send("<h1>Hi I am Rushikesh Profile</h1>")
+    middle
+})
+
+app.get("/about", (req, res)=>{
+    res.send("<h1>Hi I am Rushikesh About</h1>")
     middle
 })
 app.listen(5000);
