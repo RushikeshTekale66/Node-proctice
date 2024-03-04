55ea0dd450144c51b8cba982a816ff7022ffe63c
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-03-04 20:21:38 +0530

    File upload using multer in node.js

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
 require('./connect');
-const express  = require("express");
+const express = require("express");
+const multer = require('multer');
 const app = express();
+const db = require('./db');
 
 app.use(express.json());
 
-const db = require('./db');
+const upload = multer({
+  storage: multer.diskStorage({
+    destination: function (req, file, callback) {
+      callback(null, "uploads");
+    },
+    filename: function (req, file, callback) {
+      callback(null, file.fieldname + "-" + Date.now() + ".jpg")
+    }
+  })
+}).single("user")
 
-// Search  Data from database
-// http://localhost:5000/search/Rushikesh
-app.get('/search/:key', async(req, res)=>{
-  let result = await db.find({
-    "$or":[
-      {"name":{$regex:req.params.key}}
-    ]
-  });
-  console.log(result);
-  console.log(req.params.key);
-  res.send(result)
+// "user" field name which is specified in api call in param section
+
+app.post("/upload", upload, (req, res) => {
+  res.send("File Uploaded")
 })
-
 
 app.listen(5000);
