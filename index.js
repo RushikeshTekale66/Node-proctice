edc61403e5e42ea56775636ad233a15f9338204e
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-03-04 18:48:49 +0530

    Get : Get all the data form database using Node.js & Express.js

--- index.js
+++ index.js
@@ -6,18 +6,22 @@ app.use(express.json());
 
 const db = require('./db');
 
+// Get Data from database
 app.get('/', async(req, res)=>{
   let result = await db.find({});
   console.log(result);
   res.send(result)
 })
 
+// Add data to database
 app.post('/save', async(req, res)=>{
   let result = await db(req.body);
   let data = await result.save()
   console.log(data);
+  res.send(result)
 })
 
+// Update data in database
 // http://localhost:5000/update/65bbd202c6e29766b05a67dd
 app.put("/update/:id", async(req, res)=>{
   let result = await db.updateOne(
@@ -25,14 +29,17 @@ app.put("/update/:id", async(req, res)=>{
     {$set: req.body}
   )
   console.log(result);
+  res.send(result)
 })
 
+// Delete data in databaase
 // http://localhost:5000/delete/65be29bc9d589a8d63de71aa
 app.delete("/delete/:id", async(req, res)=>{
   let result = await db.deleteOne(
     {_id:req.params.id}
   )
   console.log(result);
+  res.send(result)
 })
 
 
