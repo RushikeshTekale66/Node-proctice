a7d5faffce8b884260ed85dd75bfc0885e450007
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-03-06 18:43:02 +0530

    Event Emitter in Node

--- index.js
+++ index.js
@@ -1,29 +1,24 @@
-const os = require('os');
+const express = require('express');
+const EventEmitter = require('events');
 
-// Some os function 
+const app = express();
 
-// Gives the architecture of system EX: x64
-console.log(os.arch());
+// Creating object of an event
+const event = new EventEmitter();
 
-// Gives the free memory of system EX: 1.3540420532226562
-console.log(os.freemem()/(1024*1024*1024));
+let count = 0;
+event.on("count", () => {
+  count++;
+  console.log("Event called", count);
+})
 
-// Gives the to total ram memory EX : 7.9
-console.log(os.totalmem()/(1024*1024*1024));
+app.get('/', (req, res) => {
+  res.send("I am ok");
+  event.emit("count");
+})
 
-// Gives the host name EX: Rushi
-console.log(os.hostname());
+app.get('/total', (req, res)=>{
+  console.log(count);
+})
 
-// Gives the platform EX: x64
-console.log(os.platform());
-
-// Gives the user information
-// EX:
-// {
-//   uid: -1,
-//   gid: -1,
-//   username: 'Rushikesh Tekale',
-//   homedir: 'C:\\Users\\Rushikesh Tekale',
-//   shell: null
-// }
-console.log(os.userInfo());
+app.listen(4000);
