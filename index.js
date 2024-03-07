1e140a8e93df34350b7d7b5dee93e6edb24767c8
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-03-07 22:22:01 +0530

    JSON.stringify

--- index.js
+++ index.js
@@ -1,54 +1,10 @@
-const express = require('express');
-const jwt = require('jsonwebtoken');
-const app = express();
-const secritKey = "secritkey";
+const str = {name:"Rushikesh", lname:"Tekale"};
 
-app.get('/', (req, res)=>{
-    res.json({
-        message:"Api called"
-    })
-})
+// convert the json object in to JSON string
+const jstring = JSON.stringify(str);
 
-app.post('/login', (req, res)=>{
-    const user =  {
-        id:1,
-        username:"anil",
-        email:"rushikesh@gmail.com"
-    }
-    jwt.sign({user},secritKey, {expiresIn:'3000s'}, (err, token)=>{
-        res.json({
-            token
-        })
-    } )
-})
+// { name: 'Rushikesh', lname: 'Tekale' }
+console.log(str);
 
-app.post(('/profile'), verifyToken, (req, res)=>{
-    jwt.verify(req.token, secritKey, (err, authData)=>{
-        if(err){
-            res.send({result:"Invalid Token"})
-        }
-        else{
-            res.json({
-                message:"Profile accessed",
-                authData
-            })
-        }
-    })
-})
-
-function verifyToken(req, res, next){
-    const bearerHeader = req.headers['authorization'];
-    if(typeof bearerHeader != 'undefined'){
-        const bearer = bearerHeader.split(' ');
-        const token = bearer[1];
-        req.token=token;
-        next();
-    }
-    else{
-        res.send({
-            result:"Token is not valid"
-        })
-    }
-}
-
-app.listen(5000);
+// {"name":"Rushikesh","lname":"Tekale"}
+console.log(jstring);
