b62eb3aec238d6e4c11b62ea8f721401d5d80f86
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-10-08 10:23:55 +0530

    http module

--- index.js
+++ index.js
@@ -1,28 +1,8 @@
+const http = require("http");
 
-const express = require("express");
-const jwt = require("jsonwebtoken");
-const app = express();
-app.use(express.json());
-
-const secretKey = "Rushikesh";
-
-// const username = "Rushikesh";
-const user = {
-    fname:"Rushikesh",
-    lname:"Tekale"
-}
-
-app.get("/", (req, res)=>{
-    // Assigning data & secret key to token
-    const token = jwt.sign({user }, secretKey, {
-        expiresIn:3000
-    });
-    setTimeout(()=>{
-        console.log(token);
-    },4000)
-    return res.json(token.split("."))
+const server = http.createServer((req, res)=>{
+    res.write("Application is running");
+    res.end();
 })
 
-app.listen(5000, console.log("App is live"));
-
-
+server.listen(8080);
