8d1beedd80a3538befb0e4b1637c60d842d21999
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-10-08 10:55:17 +0530

    Create new file & append data to it using fs module

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res)=>{
-    res.write("Application is running");
-    res.end();
+    let log = `${Date.now()} : New Request received ${req.url}`
+    fs.appendFile("log.txt", log, (err, data)=>{
+        switch(req.url){
+            case '/': res.end("home page");
+            break;
+            case '/about' : res.end("About page");
+            break;
+            case '/contact': res.end("Contact page");
+            break;
+            default : res.end("Default page");
+            break;
+        }
+    })
 })
 
-server.listen(8080);
+server.listen(8080, ()=>console.log("Server is running")
+);
